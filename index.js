e8ef9dfa3d9e97420c6c16d240ee0bacb70702b7
Galvin S,M <68190778+GalvinID@users.noreply.github.com>
2020-09-29 21:04:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ var http = require('http');
 var server = http.createServer(function (req, res) {   
     
     if (req.url == "/growtopia/server_data.php") {
-		var ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
-        req.connection.remoteAddress || 
-        req.socket.remoteAddress || 
-        req.connection.socket.remoteAddress
         if(req.method == "POST") {
 	console.log('Growtopia Logs from ' + req.connection.remoteAddress + ' In route ' + req.url + '')
         res.write('server|127.0.0.1\nport|17091\ntype|1\n#maint|Simple HTTP Server by NodeJS ~GalvinID \n\nbeta_server|127.0.0.1\nbeta_port|17091\n\nbeta_type|1\nmeta|localhost\nRTENDMARKERBS1001');
