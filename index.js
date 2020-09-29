712fe2d2057797213dcb4f080cdf2134f2aa5022
Galvin S,M <68190778+GalvinID@users.noreply.github.com>
2020-09-29 21:06:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var server = http.createServer(function (req, res) {
         if(req.method == "POST") {
 	console.log('Growtopia Logs from ' + req.connection.remoteAddress + ' In route ' + req.url + '')
         res.write('server|127.0.0.1\nport|17091\ntype|1\n#maint|Simple HTTP Server by NodeJS ~GalvinID \n\nbeta_server|127.0.0.1\nbeta_port|17091\n\nbeta_type|1\nmeta|localhost\nRTENDMARKERBS1001');
-        res.end();
 		}	
 	}
 });
