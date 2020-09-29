52c44738a9c0e2040fdfe7008835e4064029e507
Galvin S,M <68190778+GalvinID@users.noreply.github.com>
2020-09-29 18:39:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ var server = http.createServer(function (req, res) {
         req.connection.remoteAddress || 
         req.socket.remoteAddress || 
         req.connection.socket.remoteAddress
-        if(req.method === "POST") {
-		console.log('Logs' + req.connection.remoteAddress)
-		} //Growtopia Logs Server
-        res.write('server|127.0.0.1\nport|17091\ntype|1\n#maint|Maintenance\n\nbeta_server|127.0.0.1\nbeta_port|17091\n\nbeta_type|1\nmeta|localhost\nRTENDMARKERBS1001');
-		}
-    else
-        res.writeHead(404);
+        if(req.method == "POST") {
+		console.log('Growtopia Logs from ' + req.connection.remoteAddress + ' In route' + req.url + '')
+        res.write('server|52.163.223.49\nport|17097\ntype|1\n#maint|Simple HTTP Server by NodeJS ~GalvinID \n\nbeta_server|127.0.0.1\nbeta_port|17091\n\nbeta_type|1\nmeta|localhost\nRTENDMARKERBS1001');
+        res.end();
+		}	
+	}
 });
 
 server.listen(80);
