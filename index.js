efcb465605e953d1df80b5d6a8168d738a416fe2
Galvin S,M <68190778+GalvinID@users.noreply.github.com>
2020-10-10 16:18:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
   
 var http = require('http'); 
+var visits = 0;
 
 var server = http.createServer(function (req, res) {   
     
     if (req.url == "/growtopia/server_data.php") {
         if(req.method == "POST") {
-	console.log('/growtopia/server_data.php [LOGS] ' + req.connection.remoteAddress)
+	visits++;
+	console.log('')
+	console.log('New connection/Growtopia Logs')
+	console.log('Total Log    : ' + visits + '')
+	console.log('IP           : ' + ipAddress)
+        console.log('Method       : ' + req.method)
+        console.log('Route        : ' + req.url)
+        console.log('Host/IP      : ' + req.headers['host'])
+        console.log('HTTP Version : ' + req.httpVersion)
+	console.log('')
+        //Player informations
+	//Coded by GalvinID
         res.write('server|127.0.0.1\nport|17091\ntype|1\n#maint|Simple HTTP Server by NodeJS ~GalvinID \n\nbeta_server|127.0.0.1\nbeta_port|17092\n\nbeta_type|1\nmeta|localhost\nRTENDMARKERBS1001');
 		res.end()
 		}	
